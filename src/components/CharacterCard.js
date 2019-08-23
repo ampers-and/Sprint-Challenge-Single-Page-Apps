90652609476d44223d57312a7175a382cfa9af88
ampersand <ampersand@ampersands-MacBook-Pro.local>
2019-08-23 11:48:43 -0700

    material UI

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,62 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import LiveTvIcon from '@material-ui/icons/LiveTv'
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+
+export default function CharacterCard(props) {
+  
+  const classes = useStyles();
+
+  // const[character, setCharacter] = useState();
+
+  const { name, picture, species, status, origin, location, episodes} = props.character;
+
+  return (
+  <Card className={classes.card}>
+      <CardActionArea>
+      <CardMedia
+          component="img"
+          alt={name}
+          height="140"
+          image={picture}
+          title={name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+          {species}: {status}
+        </Typography>
+          <Typography variant="body2" component="p">
+            Location: {location}
+            Origin: {origin}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          <LiveTvIcon />
+          Episodes
+        </Button>
+      </CardActions>
+    </Card>
+    );
+  
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,40 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import LocationCard from './LocationCard';
 
-export default function LocationsList() {}
+export default function LocationsList(props) {
+
+    const [locations, setLocations] = useState([])
+
+    useEffect(() => {
+
+        const getLocations = () => {
+
+            axios
+                .get('https://rickandmortyapi.com/api/location/')
+                .then(response => {
+                    console.log(response.data.results);
+                    setLocations(response.data.results);
+                })
+                .catch(error => {
+                    console.error('Server Error', error);
+                  });
+        }
+
+        getLocations();
+    }, []);
+
+    return(
+        <div className='location-list'>
+            {locations.map(location =>(
+                <div className='location-details' key={location.id} 
+                    name={location.name}
+                    type={location.type}
+                    dimensions={location.dimensions}
+                    residents={location.residents}>
+                    <LocationCard/>
+                </div>
+            ))}
+        </div>
+    )
+}
