661a2c95df09da5a4236116becf21b531bff1825
ampersand <ampersand@ampersands-MacBook-Pro.local>
2019-08-23 12:33:41 -0700

    character card done

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,5 @@
 import React, { useState, useEffect} from "react";
-import { Card, Icon } from 'semantic-ui-react'
-
-const extra = (
-  <a>
-    <Icon name='video' />
-    Episodes
-  </a>
-)
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 export default function CharacterCard(props) {
 
@@ -15,14 +8,24 @@ export default function CharacterCard(props) {
   const { name, picture, species, status, origin, location} = props;
 
   return (
-    <Card
-    image={picture}
-    header={name}
-    meta={species}
-    meta={status}
-    description={origin}
-    description={location}
-    />
+    <Card>
+      <Image src={picture} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Meta>
+          <span className='status'>{species}: {status}</span>
+        </Card.Meta>
+        <Card.Description>
+          Origin: {origin} <br /> Location: {location}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name='video' />
+          Episodes
+        </a>
+      </Card.Content>
+    </Card>
     );
   
 }
