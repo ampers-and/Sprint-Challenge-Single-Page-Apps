f0a47de085da900b58fde0cd4cd6a608d8ce0493
ampersand <ampersand@ampersands-MacBook-Pro.local>
2019-08-23 13:34:56 -0700

    TabNav

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,96 +1,48 @@
 import React from "react";
 
-import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import HomeIcon from '@material-ui/icons/Home';
 import TerrainIcon from '@material-ui/icons/Terrain';
 import LiveTvIcon from '@material-ui/icons/LiveTv';
 import PeopleIcon from '@material-ui/icons/People';
+import styled from 'styled-components'
 
 import { NavLink } from "react-router-dom";
 
-import CharacterList from "./CharacterList";
-import LocationsList from './LocationsList';
-import EpisodeList from './EpisodeList';
+// import WelcomePage from './WelcomePage'
+// import CharacterList from "./CharacterList";
+// import LocationsList from './LocationsList';
+// import EpisodeList from './EpisodeList';
 
-
-//Material UI
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <Typography
-        component="div"
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        <Box p={4}>{children}</Box>
-      </Typography>
-    );
-  }
-  
-  TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-  };
-  
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
-  
-  const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-      backgroundColor: theme.palette.background.paper,
-    },
-  }));
 
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
-export default function TabNav() {
-    const classes = useStyles();
-  const [value, setValue] = React.useState(0);
 
-  function handleChange(event, newValue) {
-    setValue(newValue);
-  }
+const Nav = styled.div`
+    display:flex;
+    justify-content:space-around;
+    align-items:center;
+    margin:20px;
+`;
+
+  export default function TabNav() {
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs value={value} onChange={handleChange} indicatorColor="primary"
-          textColor="primary" aria-label="simple tabs example">
-          <Tab icon={<HomeIcon/>} label="Home" {...a11yProps(0)} />
-          <Tab icon={<PeopleIcon/>} label="Characters" {...a11yProps(1)} />
-          <Tab icon={<TerrainIcon/>} label="Locations" {...a11yProps(2)} />
-          <Tab icon={<LiveTvIcon/>} label="Episodes" {...a11yProps(3)} />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <CharacterList/>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <LocationsList/>
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        <EpisodeList/>
-      </TabPanel>
+    <div>
+      <Nav>
+        <div>
+          <NavLink to="/">Home <HomeIcon/> </NavLink>
+        </div>
+        <div>
+          <NavLink to="/characters">Characters <PeopleIcon/> </NavLink>
+        </div>
+        <div>
+          <NavLink to="/locations">Locations <TerrainIcon/> </NavLink>
+        </div>
+        <div>
+          <NavLink to="/episodes">Episodes <LiveTvIcon/> </NavLink>
+        </div>
+      </Nav>
     </div>
   );
 };
