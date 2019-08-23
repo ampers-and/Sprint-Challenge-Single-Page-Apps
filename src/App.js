add85a3307e39d91df4b03ddb5b7843e12815903
ampersand <ampersand@ampersands-MacBook-Pro.local>
2019-08-23 13:50:42 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 
+import WelcomePage from './components/WelcomePage'
+import CharacterList from "./components/CharacterList";
+import LocationsList from './components/LocationsList';
+import EpisodeList from './components/EpisodeList';
+
+import { Route } from 'react-router-dom';
+
+
+
 
 export default function App() {
   return (
     <main>
       <Header />
-      <TabNav />
+      <div>
+        <TabNav />
+        <Route exact path="/" component={ WelcomePage }/>
+        <Route path="/characters" component={ CharacterList }/>
+        <Route path="/locations" component={ LocationsList }/>
+        <Route path="/episodes" component={ EpisodeList }/>
+      </div>
+      
     </main>
   );
 }
